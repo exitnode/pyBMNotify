f15c5a18adf7055cf3ee78d38985274c04910115
Michael Clemens <mail@dl6mhc.de>
2020-12-08 11:27:16 +0100

    added ignore callsign functionality

--- config.py
+++ config.py
@@ -3,9 +3,11 @@
 # adapt the following variables to your needs
 talkgroups = [91, 262] # Talkgroups to monitor
 callsigns = ["N0CALL", "N0C4LL"] # Callsigns to monitor
+noisy_calls = ["L1DHAM"] # Noisy calls signs that will be ignored
 min_duration = 2 # Min. duration of a QSO to qualify for a push notification
 min_silence = 300 # Min. time in seconds after the last QSO before a new push notification will be send
 verbose = True # Enable extra messages (console only)
 # Pushover configuration
 pushover_token = "" # Your Pushover API token
 pushover_user = "" # Your Pushover user key
+
